c4ffa25315b16e28fd812d1588d6564bfdfa7d0a
Aziz Kemal HOSCAN <kemal.hoscan@yahoo.com>
2020-03-04 17:41:13 +0000

    Need a lot of fixing in routes adn error handlers

--- app.js
+++ app.js
@@ -1,16 +1,57 @@
 const express = require('express');
-const dataJson = require('./data.json');
+const app = express();
+
+// variable name could be changed if errors occur.
+// const data = require('data.json');
 
 // Setting view engine pugs
-app.set('views', path.join(__dirname, 'views'))
+// app.set('views', path.join(__dirname, 'views'))
+app.use(express.static('public'));
 app.set('view engine', 'pug');
 
-
+// I do not understand this part thoroghly. Did we wrote this particular
+//code in order to create a fake error or is this a must part of errorhandling
+app.use((req, res, next) => {
+  console.log("Fake error");
+  const err = new Error("What da FUCK");
+  err.status = 500;
+  next(err);
+})
 // use a static route and the express.static method to serve the static files located in the public folder
-app.use(express.static('public'));
 
 
+// Index Routing
 
-// This is for later purposes
+app.get('/', function (req, res) {
+// There should be locals set here;
+  console.log("index workss")
+  res.render('index', {projects: "I am projects"});
+})
+
+app.get('/about', function (req, res) {
+  res.render('about')
+})
 
 
+// This should be a dynamic route Id needed.
+// Leave it for now.
+app.get('/projects', function (req, res) {
+  res.render('project')
+})
+
+app.use((req, res, next) => {
+  const err = new Error("Not found!")
+  err.status = 404;
+  next(err);
+})
+
+app.use('/error',(err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error');
+  next(err);
+})
+
+app.listen(3000, () => {
+  console.log("App is listening port 3000");
+});
