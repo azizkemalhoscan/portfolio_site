eb4e7423860819170c9eac09b4e685d794cb44da
Aziz Kemal HOSCAN <kemal.hoscan@yahoo.com>
2020-03-06 18:19:56 +0000

    Error handlers implemented

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+// Basic setups.
 const express = require('express');
 const app = express();
 
-// variable name could be changed if errors occur.
+// This is where we get the data to manipulate.
 const {projects} = require('./data.json');
 
 // Setting view engine pugs
@@ -10,8 +11,7 @@ app.use('/static', express.static('public'));
 // app.use('/static', express.static('images'));
 app.set('view engine', 'pug');
 
-// I do not understand this part thoroghly. Did we wrote this particular
-//code in order to create a fake error or is this a must part of errorhandling
+// Testing middleware
 // app.use((req, res, next) => {
 //   console.log("Fake error");
 //   const err = new Error("Errorrrrrr!");
@@ -21,48 +21,56 @@ app.set('view engine', 'pug');
 // use a static route and the express.static method to serve the static files located in the public folder
 
 
-// Index Routing
+// Index Routing local are set and sent.
 
 app.get('/', function (req, res) {
-// There should be locals set here;
-// I DONT KNOW HOW TO DEFINE LOCALS
-  // res.locals.projects = data.projects;
+
   console.log("index workss");
   res.render('index', {projects});
 })
-
+// This is for rendering about page when clicked.
 app.get('/about', function (req, res) {
   res.render('about')
 })
 
-
-// This should be a dynamic route Id needed.
-// Leave it for now.
-// CONSIDER THIS CODE AGAIN/ SHOULD IT BE HERE OR ALL ROUTES SHOULD BE IN A DIFFERENT FOLDER. or is it even true :)
+/*
+Dynamic routes implemented. Whenever a project selected by the user, projects page will be rendered
+specifically for that project.
+*/
 app.get('/projects/:id', function (req, res, next) {
   const projectId = req.params.id;
   const project = projects.find(({id}) => id === +projectId);
   if(project){
     res.render('project', {project})
   } else {
-    res.sendStatus(404);
+    res.render('error');
   }
 })
 
+
+
+// This is an error middleware.
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error');
+  next(err);
+})
+
+
+// This is arrow middleware for handling non-existing url's. --404--
 app.use((req, res, next) => {
   const err = new Error("There is no such url BABE!")
+  res.locals.error = err;
   err.status = 404;
+  res.render('error')
   // console.log(err.status);
   next(err);
 })
 
-app.use('/error',(err, req, res, next) => {
-  res.locals.error = err;
-  res.status(err.status);
-  res.send('OOOOooopsy sorry an error occured');
-  next(err);
-})
 
+
+// This is for defining where to run this page on local server. It is set to 3000.
 app.listen(3000, () => {
   console.log("App is listening port 3000");
 });
